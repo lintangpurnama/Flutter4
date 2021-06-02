b16022926950c9a33ba960e6075976c44a5bfbb2
lintang purnama <lintangpurnama1@gmail.com>
2021-06-02 21:54:55 +0700

    navbar

---
+++ android/app/src/main/kotlin/com/example/tugas7_nav/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas7_nav
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/hal_headset.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Headset extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            new Padding(padding: new EdgeInsets.all(20.0)),
+            new Text(
+              "HEADSET",
+              style: new TextStyle(fontSize: 30.0),
+            ),
+            new Padding(padding: new EdgeInsets.all(20.0)),
+            new Icon(
+              Icons.headset_mic_rounded,
+              size: 90.0,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
